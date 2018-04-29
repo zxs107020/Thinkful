6ac6aa482fb7df4809958c4bf291ab856122e0f9
zxs107020 <zxs107020@utdallas.edu>
2018-04-29 15:45:41 -0500

    Update 05_segmentation.py

--- 05_segmentation.py
+++ 05_segmentation.py
@@ -16,7 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the variables
-wd = '/Users/zansadiq/Documents/Code/local/segmentation'
+wd = '/path/to/files'
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank-additional.zip'
 fn = 'bank-additional.zip'
 path = 'staging/bank-additional'
